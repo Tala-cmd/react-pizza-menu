f24525dfe9a3920714e76e3a72f098c69cb2c8bd
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-11-13 18:59:43 +0300

    Create the folder

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Spinach Pizza",
+    ingredients: "Tomato, mozzarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+
+export default function App() {
+  
+  return (
+    <>
+      <h1>Tala's pizza menu</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </>
+  );
+}
+
+export function Pizza(){
+  return(
+    <>
+    <img src="pizzas/spinach.jpg" alt="Spinach pizza" />
+    <h2>Spinach Pizza </h2>
+    <p>Tomato, mozzarella, spinach, and ricotta cheese</p>
+  </>
+  
+  )
+}
+
+
+
