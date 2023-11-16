e6b36f89ad19e8d73d5f01cee7aabdad0b95fdb9
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-11-16 14:39:20 +0300

    Extracting JSX into a new component

--- src/App.jsx
+++ src/App.jsx
@@ -79,9 +79,7 @@ function Menu() {
 
 export function Pizza(props) {
   console.log(props);
-
   if(props.pizzaObject.soldOut) return null;
-
 
   return (
     <li className="pizza">
@@ -116,12 +114,20 @@ function Footer() {
 
   return (
     <footer className="footer">
-      <div className="order">
-        {isOpen ? <p>We're open until {closeHour}:00. Come visit us or order online.</p> : 
-        <p>We're happy to welcome you between {openHour}:00. and {closeHour}:00.</p>
-        }
-        <button className="btn">Order</button>
-      </div>
+      {isOpen ? (
+        <Order closeHour={closeHour} />
+        ) : ( 
+        <p>We're happy to welcome you between {openHour}:00. and {closeHour}:00.</p> )
+      }
     </footer>
   );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>We're open until {props.closeHour}:00. Come visit us or order online.</p>
+      <button className="btn">Order</button> 
+    </div>
+    )
 }
